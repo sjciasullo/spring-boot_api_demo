639d1ae98cfe33cd2cd0457a2f29719f25634d58
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-25 14:57:34 -0400

    create some user seed data

---
+++ spring-api/src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,7 @@
+INSERT INTO USERS
+	(USER_NAME, TAG_LINE)
+VALUES
+	('the first', 'each moment begin again'),
+	('another user', 'every day is another day'),
+	('someone', 'who am i, someone at all?'),
+	('someone else', 'or am i just someone else?');
