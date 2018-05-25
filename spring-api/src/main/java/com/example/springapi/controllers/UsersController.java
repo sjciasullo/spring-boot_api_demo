4c7a256bd48959a43fb168200e92d50886d8c61e
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-25 15:45:02 -0400

    wire up rest route to findAllUsers

--- spring-api/src/main/java/com/example/springapi/controllers/UsersController.java
+++ spring-api/src/main/java/com/example/springapi/controllers/UsersController.java
@@ -1,8 +1,15 @@
 package com.example.springapi.controllers;
 
+import com.example.springapi.models.User;
+
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UsersController {
-    
+    @GetMapping("/users")
+    public List<User> findAllUsers() {
+        return new ArrayList<User>();
+    }
 }
