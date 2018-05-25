f78584b2fb23e7759fbfe0de0e25f027d6b98c9e
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-25 15:51:03 -0400

    autowire interface with controller to talk to db

--- spring-api/src/main/java/com/example/springapi/controllers/UsersController.java
+++ spring-api/src/main/java/com/example/springapi/controllers/UsersController.java
@@ -1,15 +1,20 @@
 package com.example.springapi.controllers;
 
 import com.example.springapi.models.User;
-
+import com.example.springapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UsersController {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/users")
-    public List<User> findAllUsers() {
-        return new ArrayList<User>();
+    public Iterable<User> findAllUsers() {
+        return userRepository.findAll();
     }
 }
