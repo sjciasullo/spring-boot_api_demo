91a808fa2e5d0ef5667da3f4a35f8c9b0e6c536e
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-27 18:51:42 -0400

    add seed data for activites table

--- spring-api/src/main/resources/db/migration/afterMigrate.sql
+++ spring-api/src/main/resources/db/migration/afterMigrate.sql
@@ -1,7 +1,23 @@
 INSERT INTO USERS
 	(USER_NAME, TAG_LINE)
 VALUES
-	('the first', 'each moment begin again'),
+	('first_user', 'each moment begin again'),
 	('another user', 'every day is another day'),
 	('someone', 'who am i, someone at all?'),
-	('someone else', 'or am i just someone else?');
+	('kungfu', 'dream is more in the process');
+
+INSERT INTO ACTIVITIES
+    (ACTIVITY_NAME, USER_ID, MONTH, TOTAL_MINUTES, NOTES)
+VALUES
+    ('meditation', 4, 'january', 930, 'meditate for 30 minutes daily'),
+    ('exercise', 4, 'january', 200, 'workout'),
+    ('musick', 4, 'january', 9300, 'practice musicking'),
+    ('meditation', 4, 'february', 930, 'meditate for 30 minutes daily'),
+    ('exercise', 4, 'february', 200, 'workout'),
+    ('musick', 4, 'february', 9300, 'practice musicking'),
+    ('meditation', 4, 'march', 930, 'meditate for 30 minutes daily'),
+    ('exercise', 4, 'march', 200, 'workout'),
+    ('musick', 4, 'march', 9300, 'practice musicking'),
+    ('meditation', 4, 'april', 930, 'meditate for 30 minutes daily'),
+    ('exercise', 4, 'april', 200, 'workout'),
+    ('musick', 4, 'april', 9300, 'practice musicking');
