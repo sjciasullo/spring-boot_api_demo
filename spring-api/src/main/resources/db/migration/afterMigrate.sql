e7f55067370c8af02cb698fc4f7b8f1203384622
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-27 21:03:45 -0400

    add dummy data for another user

--- spring-api/src/main/resources/db/migration/afterMigrate.sql
+++ spring-api/src/main/resources/db/migration/afterMigrate.sql
@@ -20,4 +20,16 @@ VALUES
     ('musick', 4, 'march', 9800, 'practice musicking'),
     ('meditation', 4, 'april', 1110, 'meditate for 30 minutes daily'),
     ('exercise', 4, 'april', 667, 'workout'),
-    ('musick', 4, 'april', 10000, 'practice musicking');
+    ('musick', 4, 'april', 10000, 'practice grokking'),
+    ('meditation', 1, 'january', 200, 'meditate for 30 minutes daily'),
+    ('exercise', 1, 'january', 1000, 'workout'),
+    ('grok', 1, 'january', 500, 'practice grokking'),
+    ('meditation', 1, 'february', 450, 'meditate for 30 minutes daily'),
+    ('exercise', 1, 'february', 1100, 'workout'),
+    ('grok', 1, 'february', 600, 'practice grokking'),
+    ('meditation', 1, 'march', 340, 'meditate for 30 minutes daily'),
+    ('exercise', 1, 'march', 2000, 'workout'),
+    ('grok', 1, 'march', 900, 'practice grokking'),
+    ('meditation', 1, 'april', 110, 'meditate for 30 minutes daily'),
+    ('exercise', 1, 'april', 2500, 'workout'),
+    ('grok', 1, 'april', 80, 'practice grokking');
