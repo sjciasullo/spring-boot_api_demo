10fd94d902208ab188bf5c43c6f4e3c7db5d83e3
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-28 16:33:42 -0400

    add cross-origin support to controllers

--- spring-api/src/main/java/com/example/springapi/controllers/ActivitiesController.java
+++ spring-api/src/main/java/com/example/springapi/controllers/ActivitiesController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ActivitiesController {
 
     @Autowired
--- spring-api/src/main/java/com/example/springapi/controllers/UsersController.java
+++ spring-api/src/main/java/com/example/springapi/controllers/UsersController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UsersController {
 
     @Autowired
