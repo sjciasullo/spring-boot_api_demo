1571da37f18d68f66d5705d47400868fb7e97c3a
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-27 20:34:43 -0400

    add variation to seed points

--- spring-api/src/main/resources/db/migration/afterMigrate.sql
+++ spring-api/src/main/resources/db/migration/afterMigrate.sql
@@ -10,14 +10,14 @@ INSERT INTO ACTIVITIES
     (ACTIVITY_NAME, USER_ID, MONTH, TOTAL_MINUTES, NOTES)
 VALUES
     ('meditation', 4, 'january', 930, 'meditate for 30 minutes daily'),
-    ('exercise', 4, 'january', 200, 'workout'),
+    ('exercise', 4, 'january', 500, 'workout'),
     ('musick', 4, 'january', 9300, 'practice musicking'),
-    ('meditation', 4, 'february', 930, 'meditate for 30 minutes daily'),
-    ('exercise', 4, 'february', 200, 'workout'),
-    ('musick', 4, 'february', 9300, 'practice musicking'),
-    ('meditation', 4, 'march', 930, 'meditate for 30 minutes daily'),
-    ('exercise', 4, 'march', 200, 'workout'),
-    ('musick', 4, 'march', 9300, 'practice musicking'),
-    ('meditation', 4, 'april', 930, 'meditate for 30 minutes daily'),
-    ('exercise', 4, 'april', 200, 'workout'),
-    ('musick', 4, 'april', 9300, 'practice musicking');
+    ('meditation', 4, 'february', 1000, 'meditate for 30 minutes daily'),
+    ('exercise', 4, 'february', 400, 'workout'),
+    ('musick', 4, 'february', 9500, 'practice musicking'),
+    ('meditation', 4, 'march', 800, 'meditate for 30 minutes daily'),
+    ('exercise', 4, 'march', 300, 'workout'),
+    ('musick', 4, 'march', 9800, 'practice musicking'),
+    ('meditation', 4, 'april', 1110, 'meditate for 30 minutes daily'),
+    ('exercise', 4, 'april', 667, 'workout'),
+    ('musick', 4, 'april', 10000, 'practice musicking');
