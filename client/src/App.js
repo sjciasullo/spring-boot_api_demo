791cd69e01129c21801b64c23f64b296352edc7e
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-28 17:53:26 -0400

    fetch users upon app component mounting

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,29 @@ import Header from './components/Header';
 import './styles/App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      loaded: false,
+      users: [],
+    }
+  }
+
+  componentDidMount(){
+    this.getUsers();
+  }
+
+  // Calls to API
+  getUsers(){
+    fetch('http://localhost:8080/users', {
+      method: 'GET'
+    }).then(res => res.json())
+    .then(json => {
+      console.log(json);
+      this.setState({users: json})
+    }).catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
