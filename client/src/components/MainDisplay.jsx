595b462b7e9744fe95a950e8e864cea324da2c79
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-29 07:49:33 -0400

    begin mainDisplay

---
+++ client/src/components/MainDisplay.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class MainDisplay extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+
+    }
+  }
+
+  render(){
+    return(
+      <div className="main-display">
+
+      </div>
+    )
+  }
+}
+
+export default MainDisplay;
