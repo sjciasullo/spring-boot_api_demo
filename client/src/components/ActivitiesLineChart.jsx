50d02c9c9533f16b581bca330b4ed87b2f5d92ac
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-28 21:30:25 -0400

    use dummy data with line chart

--- client/src/components/ActivitiesLineChart.jsx
+++ client/src/components/ActivitiesLineChart.jsx
@@ -6,17 +6,23 @@ function ActivitesLinechart(props){
 
   //format activites for linechart data
 
+  const data = [
+    {month: 'jan', sports: 80, music: 50}, 
+    {month: 'feb', sports: 110, music: 100}, 
+    {month: 'march', sports: 50, music: 200}
+  ];
+
   //this is currently an example from recharts.org
   return (
-    <LineChart width={730} height={250} data={activities}
+    <LineChart width={730} height={250} data={data}
                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
+      <XAxis dataKey="month" />
       <YAxis />
       <Tooltip />
       <Legend />
-      <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      <Line type="monotone" dataKey="sports" stroke="#8884d8" />
+      <Line type="monotone" dataKey="music" stroke="#82ca9d" />
     </LineChart>
   )
 }
