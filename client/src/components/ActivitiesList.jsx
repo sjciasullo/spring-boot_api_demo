1867efef5e7852ab8dec8f5c5fce778797aed2fa
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-29 09:26:53 -0400

    style list tags max height and border

--- client/src/components/ActivitiesList.jsx
+++ client/src/components/ActivitiesList.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import '../styles/ActivitiesList.css';
 
 function ActivitiesList(props){
   const activities = props.activities;
 
   const activitiesListTags = activities.map((activity) => {
     return (
-      <li key={activity.id}>{activity.activityName} for {activity.totalMinutes} minutes in {activity.month}</li>
+      <li key={activity.id}>
+      {activity.activityName} for {activity.totalMinutes} minutes in {activity.month}
+      </li>
     )
   })
 
   return (
     <div className="activities-list">
       <h2>Activity Log</h2>
-      <ul>
-        {activitiesListTags}
-      </ul>
+      <div className="list-tags-container">
+        <ul>
+          {activitiesListTags}
+        </ul>
+      </div>
     </div>
   )
 }
