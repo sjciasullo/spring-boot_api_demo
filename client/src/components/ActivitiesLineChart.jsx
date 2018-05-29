02ee23cc1b65afb74346a10fd490e860bb936ae0
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-28 21:17:20 -0400

    import all components properly

--- client/src/components/ActivitiesLineChart.jsx
+++ client/src/components/ActivitiesLineChart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LineChart from 'recharts';
+import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line} from 'recharts';
 
 function ActivitesLinechart(props){
   const activities = props.activities;
