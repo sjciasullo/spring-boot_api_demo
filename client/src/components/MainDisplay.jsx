c4c99ca7e11e9015305fcc53ea3827edad1f93ba
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-30 19:33:28 -0400

    flex activities container

--- client/src/components/MainDisplay.jsx
+++ client/src/components/MainDisplay.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import '../styles/MainDisplay.css';
+
 import ActivitiesLinechart from './ActivitiesLineChart';
 import ActivitiesList from './ActivitiesList';
 import ActivityForm from './ActivityForm';
