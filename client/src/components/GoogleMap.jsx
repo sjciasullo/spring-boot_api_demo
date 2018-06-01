c8a31187ccb0a205d9083cc42fad5bfbca16be60
Steven Ciasullo <sjciasullo@gmail.com>
2018-06-01 14:45:04 -0400

    map renders with correct pseudo sizing

--- client/src/components/GoogleMap.jsx
+++ client/src/components/GoogleMap.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../styles/GoogleMap.css';
 
 function GoogleMap(props) {
   // will take array of locations as a prop
   
   return(
-    <div class='map-container'>
+    <div className='map-container'>
       <p>This will be a map</p>
     </div>
   )
