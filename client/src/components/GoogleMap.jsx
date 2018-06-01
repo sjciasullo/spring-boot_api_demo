8a80ee286190802cd1df6a0bed1a5e52ca0adb4a
Steven Ciasullo <sjciasullo@gmail.com>
2018-06-01 15:11:09 -0400

    add dummy API key

--- client/src/components/GoogleMap.jsx
+++ client/src/components/GoogleMap.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/GoogleMap.css';
 
+const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+
 function GoogleMap(props) {
   // will take array of locations as a prop
-  
+  console.log(API_KEY);
   return(
     <div className='map-container'>
       <p>This will be a map</p>
