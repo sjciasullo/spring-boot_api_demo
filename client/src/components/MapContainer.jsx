f9675919c3b16dbb56444c88f8131dbd78131ce9
Steven Ciasullo <sjciasullo@gmail.com>
2018-06-03 16:31:08 -0400

    scale down component did mount

--- client/src/components/MapContainer.jsx
+++ client/src/components/MapContainer.jsx
@@ -4,6 +4,14 @@ import '../styles/MapContainer.css';
 
 const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
+// dummy data
+const POINTS = [
+  {lat: 25.774, lng: -80.190},
+  {lat: 18.466, lng: -66.118},
+  {lat: 32.321, lng: -64.757},
+  {lat: 25.774, lng: -80.190}
+]
+
 // using method from https://www.npmjs.com/package/google-maps-react
 
 export class MapContainer extends Component {
@@ -16,35 +24,16 @@ export class MapContainer extends Component {
   // and in map marker creating can specify name, position{lat, long}, title
 
   componentDidMount(){
-    const points = [
-      {lat: 25.774, lng: -80.190},
-      {lat: 18.466, lng: -66.118},
-      {lat: 32.321, lng: -64.757},
-      {lat: 25.774, lng: -80.190}
-    ]
-    var bounds = new this.props.google.maps.LatLngBounds();
-
-    // map through points to create markers while extending the bounds on map for each
-    const markerComponents = points.map((point, index) =>{
-      bounds.extend(point);
-    })
-    this.refs.mapper.map.fitBounds(bounds);
+    this.refs.mapper.map.fitBounds(this.refs.mapper.props.bounds);
   }
 
   render() {
-    // dummy data
-    const points = [
-      {lat: 25.774, lng: -80.190},
-      {lat: 18.466, lng: -66.118},
-      {lat: 32.321, lng: -64.757},
-      {lat: 25.774, lng: -80.190}
-    ]
-
+    
     // create bounds object for setting the map bounds
-    var bounds = new this.props.google.maps.LatLngBounds();
+    let bounds = new this.props.google.maps.LatLngBounds();
 
     // map through points to create markers while extending the bounds on map for each
-    const markerComponents = points.map((point, index) =>{
+    const markerComponents = POINTS.map((point, index) =>{
       bounds.extend(point);
       return (
         <Marker key={index} name={"point" + index} position={point} />
