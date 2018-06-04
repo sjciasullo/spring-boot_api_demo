b609911a0d9705abc7c6129c71a6553dcb37fc0c
Steven Ciasullo <sjciasullo@gmail.com>
2018-06-04 13:07:23 -0400

    convert locationInput into functional component

--- client/src/components/LocationInput.jsx
+++ client/src/components/LocationInput.jsx
@@ -1,55 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PlacesAutocomplete from 'react-places-autocomplete';
 // uses example from package https://www.npmjs.com/package/react-places-autocomplete
 
 import '../styles/LocationInput.css';
 
-class LocationInput extends Component {
-  // this could be functional component once state is handled above
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
-      <PlacesAutocomplete
-        value={this.props.location} // this will come from props
-        onChange={this.props.handleChange} // these might be func
-        onSelect={this.props.handleSelect}
-        googleCallbackName="initGoogle"
-      >
-        {/* handle input and autocomplete components */}
-        {({ getInputProps, suggestions, getSuggestionItemProps }) => (
-          <div>
-            <input
-              {...getInputProps({
-                placeholder: "Location",
-                className: "location-search-input"
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {suggestions.map(suggestion => {
-                const className = suggestion.active ? "suggestion-item--active" : "suggestion-item";
-                const style = suggestion.active
-                  ? { backgroundColor: "#ccc", cursor: "pointer" }
-                  : { backgroundColor: "#fff", cursor: "pointer" };
-                style.width = "350px";
-                style.padding = "5px";
-                style.fontSize = "9px";
-                return (
-                  <div {...getSuggestionItemProps(suggestion, { className, style })}>
-                    <span>{suggestion.description}</span>
-                  </div>
-                )
-              })}
-            </div>
+function LocationInput(props){
+  return (
+    <PlacesAutocomplete
+      value={props.location} // this will come from props
+      onChange={props.handleChange} // these might be func
+      onSelect={props.handleSelect}
+      googleCallbackName="initGoogle"
+    >
+      {/* handle input and autocomplete components */}
+      {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+        <div>
+          <input
+            {...getInputProps({
+              placeholder: "Location",
+              className: "location-search-input"
+            })}
+          />
+          <div className="autocomplete-dropdown-container">
+            {suggestions.map(suggestion => {
+              const className = suggestion.active ? "suggestion-item--active" : "suggestion-item";
+              const style = suggestion.active
+                ? { backgroundColor: "#ccc", cursor: "pointer" }
+                : { backgroundColor: "#fff", cursor: "pointer" };
+              style.width = "350px";
+              style.padding = "5px";
+              style.fontSize = "9px";
+              return (
+                <div {...getSuggestionItemProps(suggestion, { className, style })}>
+                  <span>{suggestion.description}</span>
+                </div>
+              )
+            })}
           </div>
-        )} 
-      </PlacesAutocomplete>
-    );
-  }
+        </div>
+      )} 
+    </PlacesAutocomplete>
+  );
 }
 
 export default LocationInput;
