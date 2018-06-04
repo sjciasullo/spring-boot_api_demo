48e1a2d1140c80f4c46f825b42d0e6ea97ba74e1
Steven Ciasullo <sjciasullo@gmail.com>
2018-06-04 14:32:14 -0400

    refactor app name change

--- spring-api/src/test/java/com/example/springapi/ActivityTrackerTests.java
+++ spring-api/src/test/java/com/example/springapi/ActivityTrackerTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class ActivityTrackerTests {
 
 	@Test
 	public void contextLoads() {
